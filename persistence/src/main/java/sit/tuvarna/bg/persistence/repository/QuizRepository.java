a9a2cb174614965f2036f9e7718bdab52a3c7ba4
Ivaylo Petkov <ivi_petkow@abv.bg>
2024-02-25 21:06:02 +0200

    Refactor quiz category

--- persistence/src/main/java/sit/tuvarna/bg/persistence/repository/QuizRepository.java
+++ persistence/src/main/java/sit/tuvarna/bg/persistence/repository/QuizRepository.java
@@ -2,6 +2,7 @@ package sit.tuvarna.bg.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sit.tuvarna.bg.persistence.entity.Category;
 import sit.tuvarna.bg.persistence.entity.Quiz;
 import sit.tuvarna.bg.persistence.enums.QuizCategory;
 
@@ -10,7 +11,7 @@ import java.util.UUID;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, UUID> {
-    List<Quiz> findAllByCategory(QuizCategory category);
+    List<Quiz> findAllByCategory(Category category);
 
     List<Quiz> findAllByIsRequestedIsTrue();
 }
