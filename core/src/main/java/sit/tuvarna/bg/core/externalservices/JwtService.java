ece7aa6046bd7f82e3894b5aaa8b11e9aef4c2e7
Ivaylo Petkov <ivi_petkow@abv.bg>
2024-03-07 20:48:25 +0200

    Security refactor

--- core/src/main/java/sit/tuvarna/bg/core/externalservices/JwtService.java
+++ core/src/main/java/sit/tuvarna/bg/core/externalservices/JwtService.java
@@ -61,7 +61,6 @@ public class JwtService {
     private boolean isTokenExpired(String jwtToken) {
         return extractExpiration(jwtToken).before(new Date());
     }
-    //test
 
     public boolean isTokenRevoked(String jwtToken) {
         return tokenRepository.findByToken(jwtToken)
