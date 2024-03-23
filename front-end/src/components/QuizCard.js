a24d288c722645418e87b25a08768c6d05864cc7
Ivaylo Petkov <ivi_petkow@abv.bg>
2024-03-23 12:46:17 +0200

    finalize quiz card

--- front-end/src/components/QuizCard.js
+++ front-end/src/components/QuizCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/quizCard.css';
 
-const QuizCard = ({ quiz }) => {
+const QuizCard = ({ quiz, onClick }) => {
     const statusText = quiz.haveBeenPassed ? 'Взет' :
                        quiz.haveBeenCompleted ? 'Решен' : 'Нерешаван';
     const statusClass = quiz.haveBeenPassed ? 'status-passed' :
@@ -9,12 +9,13 @@ const QuizCard = ({ quiz }) => {
     const noDataText = "0";
 
     return (
-        <div className="row quiz-card">
-            <div className={`col-5 quiz-item ${statusClass}`}>{quiz.name}</div>
-            <div className="col-2 quiz-item">{quiz.category}</div>
+        <div className={`row quiz-card ${statusClass}`} onClick={() => onClick(quiz)}>
+            <div className="col-4 quiz-item text-start">{quiz.name}</div>
+            <div className="col-2 quiz-item text-start">{quiz.category}</div>
             <div className="col-1 quiz-item">{quiz.questionsCount}</div>
-            <div className="col-1 quiz-item">{quiz.averageSecondsNeeded ?? noDataText}</div>
+            <div className="col-1 quiz-item">{quiz.averageSecondsNeeded ?? noDataText} сек.</div>
             <div className="col-1 quiz-item">{quiz.averageCorrectAnswers ?? noDataText}</div>
+            <div className="col-1 quiz-item">{quiz.personalBestXpGained ?? noDataText}/100</div>
             <div className="col-2 quiz-item">{statusText}</div>
         </div>
     );
--- front-end/src/pages/AllQuizzes.js
+++ front-end/src/pages/AllQuizzes.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import QuizCard from '../components/QuizCard';
 import '../styles/allQuizzes.css';
 
 const AllQuizzes = ({ email, token }) => {
     const [quizzes, setQuizzes] = useState([]);
+    const [selectedQuiz, setSelectedQuiz] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchQuizzes = async () => {
@@ -28,21 +31,64 @@ const AllQuizzes = ({ email, token }) => {
         fetchQuizzes();
     }, [email, token]);
 
-    console.log(quizzes);
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.key === 'Escape') {
+                handleCloseModal();
+            }
+        };
+
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    const handleCloseModal = () => {
+        setSelectedQuiz(null);
+    };
+
+    const handleStartQuiz = (quizId) => {
+        navigate(`/start-quiz/${quizId}`);
+    };
 
     return (
         <div className="container all-quizzes-container align-items-center">
             <div className="row quiz-header pt-2">
-                <div className="col-5 header">Име</div>
+                <div className="col-4 header">Име</div>
                 <div className="col-2 header">Категория</div>
                 <div className="col-1 header">Въпроси</div>
                 <div className="col-1 header">Средно време</div>
                 <div className="col-1 header">Среден резултат</div>
+                <div className="col-1 header">Мой опит</div>
                 <div className="col-2 header">Статус</div>
             </div>
             {quizzes.map(quiz => (
-                <QuizCard key={quiz.quizId} quiz={quiz} />
+                <div className="all-quiz-card" key={quiz.quizId}>
+                    <QuizCard quiz={quiz}  onClick={() => setSelectedQuiz(quiz)} />
+                </div>
             ))}
+            {selectedQuiz && (
+                <div className="dialog-overlay" onClick={handleCloseModal}>
+                    <dialog open className="quiz-dialog" onClick={e => e.stopPropagation()}>
+                        <h3 className='text-center'>{selectedQuiz.name}</h3>
+                        <p className='mt-2'>Категория: {selectedQuiz.category}</p>
+                        <p>Въпроси: {selectedQuiz.questionsCount}</p>
+                        <p className='mt-2'>Средно време: {selectedQuiz.averageSecondsNeeded ?? 0}</p>
+                        <p>Среден резултат: {selectedQuiz.averageCorrectAnswers ?? 0}</p>
+                        <p className='mt-2'>Мой най-добър резултат: {selectedQuiz.personalBestCorrectAnswers ?? 0}</p>
+                        <p>Мое най-бързо време: {selectedQuiz.personalBestTime ?? 0}</p>
+                        <p>Мой опит: {selectedQuiz.personalBestXpGained ?? 0}/100</p>
+
+                        <p className='mt-2 text-center'>При натискане на бутона "Започни" ще започне да тече вашето време</p>
+                        <div className="dialog-buttons-container">
+                            <button className='btn-cancel' onClick={handleCloseModal}>Затвори</button>
+                            <button className='btn-start' onClick={() => handleStartQuiz(selectedQuiz.quizId)}>Започни</button>
+                        </div>
+                    </dialog>
+                </div>
+            )}
         </div>
     );
 };
