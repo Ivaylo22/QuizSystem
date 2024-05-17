3b9d9c8734790b44839d47258d1a5dacd3ac3974
Ivaylo Petkov <ivi_petkow@abv.bg>
2024-05-17 12:20:40 +0300

    Create getPublicTests controller

--- rest/src/main/java/sit/tuvarna/bg/rest/controller/TestController.java
+++ rest/src/main/java/sit/tuvarna/bg/rest/controller/TestController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import sit.tuvarna.bg.api.operations.test.create.CreateTestOperation;
 import sit.tuvarna.bg.api.operations.test.create.CreateTestRequest;
 import sit.tuvarna.bg.api.operations.test.create.CreateTestResponse;
+import sit.tuvarna.bg.api.operations.test.getpublictests.GetPublicTestsOperation;
+import sit.tuvarna.bg.api.operations.test.getpublictests.GetPublicTestsRequest;
+import sit.tuvarna.bg.api.operations.test.getpublictests.GetPublicTestsResponse;
 import sit.tuvarna.bg.api.operations.test.getsubjects.GetSubjectsOperation;
 import sit.tuvarna.bg.api.operations.test.getsubjects.GetSubjectsRequest;
 import sit.tuvarna.bg.api.operations.test.getsubjects.GetSubjectsResponse;
@@ -19,11 +22,18 @@ public class TestController {
 
     private final CreateTestOperation createTest;
     private final GetSubjectsOperation getSubjects;
+    private final GetPublicTestsOperation getPublicTests;
 
     @GetMapping("/subjects")
     public ResponseEntity<GetSubjectsResponse> getSubjects() {
         GetSubjectsRequest request = GetSubjectsRequest.builder().build();
         return new ResponseEntity<>(getSubjects.process(request), HttpStatus.OK);
+    }
+
+    @GetMapping("/get-public-tests")
+    public ResponseEntity<GetPublicTestsResponse> getPublicTests() {
+        GetPublicTestsRequest request = GetPublicTestsRequest.builder().build();
+        return new ResponseEntity<>(getPublicTests.process(request), HttpStatus.OK);
     }
 
     @PostMapping("/create")
